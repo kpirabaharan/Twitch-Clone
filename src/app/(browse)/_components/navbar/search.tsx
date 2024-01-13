ebfefaca4e446a22d5aa628a3f9a137f9d943e40
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-01-13 13:51:45 -0500

    Add search functionality to navbar
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/app/(browse)/_components/navbar/search.tsx
+++ src/app/(browse)/_components/navbar/search.tsx
@@ -1,26 +1,59 @@
 'use client';
 
-import { SearchIcon } from 'lucide-react';
+import { SearchIcon, X } from 'lucide-react';
+import qs from 'query-string';
+import { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { useRouter } from 'next/navigation';
 
 export const Search = () => {
+  const router = useRouter();
+  const [value, setValue] = useState('');
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    const url = qs.stringifyUrl(
+      {
+        url: '/search',
+        query: { term: value },
+      },
+      { skipEmptyString: true },
+    );
+
+    router.push(url);
+  };
+
+  const onClear = () => setValue('');
+
   return (
-    <form className='flex flex-row items-center'>
+    <form onSubmit={onSubmit} className='relative flex flex-row items-center'>
       <Input
+        value={value}
+        onChange={e => setValue(e.target.value)}
         placeholder='Search'
         className='rounded-r-none border-0 outline-none 
         focus-visible:ring-0 focus-visible:ring-transparent 
         focus-visible:ring-offset-0'
       />
+      {value && (
+        <X
+          onClick={onClear}
+          className='absolute right-12 h-5 w-5 cursor-pointer transition 
+          hover:opacity-75'
+        />
+      )}
       <Button
         type={'submit'}
-        className='rounded-l-none'
+        className='group rounded-l-none'
         size={'icon'}
         variant={'secondary'}
       >
-        <SearchIcon className='h-5 w-5' />
+        <SearchIcon className='h-5 w-5 group-hover:opacity-75' />
       </Button>
     </form>
   );
