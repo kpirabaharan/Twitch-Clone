460d0c6b093a5ba06a1f252f49e149db243fb995
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-01-22 14:43:17 -0500

    Nav Item -> Button
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/app/(dashboard)/u/[username]/(home)/_components/sidebar/nav-item.tsx
+++ src/app/(dashboard)/u/[username]/(home)/_components/sidebar/nav-item.tsx
@@ -3,6 +3,7 @@
 import type { LucideIcon } from 'lucide-react';
 
 import { MotionDiv } from '@/components/framer/motion-div';
+import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { useCreatorSidebar } from '@/store/use-creator-sidebar';
 import { navItemVariants } from './animations';
@@ -23,19 +24,21 @@ export const NavItem = ({
   const { isExpanded } = useCreatorSidebar();
 
   return (
-    <MotionDiv
-      initial={'closed'}
-      animate={isExpanded ? 'open' : 'closed'}
-      exit={isExpanded ? 'open' : 'closed'}
-      variants={navItemVariants}
-      className={cn(
-        'overflow-hidde flex flex-row items-center justify-between gap-x-8 px-[10px]',
-      )}
-    >
-      <div className='inline-flex min-h-10 min-w-10 items-center justify-center'>
-        <Icon className='h-5 w-5' />
-      </div>
-      <p className='overflow-hidden'>{label}</p>
-    </MotionDiv>
+    <Button variant={'ghost'} asChild>
+      <MotionDiv
+        initial={'closed'}
+        animate={isExpanded ? 'open' : 'closed'}
+        exit={isExpanded ? 'open' : 'closed'}
+        variants={navItemVariants}
+        className={cn(
+          'flex flex-row items-center justify-between gap-x-8 px-[10px]',
+        )}
+      >
+        <div className='inline-flex min-h-10 min-w-10 items-center justify-center'>
+          <Icon className='h-5 w-5' />
+        </div>
+        <p className='overflow-hidden'>{label}</p>
+      </MotionDiv>
+    </Button>
   );
 };
