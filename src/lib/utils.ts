134cb29d02e68facb9b1dd847ce79a6ca1001dd5
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-01-28 20:53:00 -0500

    Colored Named Hashing (Scaled to Higher Values)
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,26 +1,33 @@
 import { clsx, type ClassValue } from 'clsx';
+import stc from 'string-to-color';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-
 export const stringToColor = (str: string) => {
-  let hash = 0;
-  let i;
-  let chr;
-  let len;
+  const scaleValue = (value: number, min: number, max: number) => {
+    return Math.floor((value / 100) * (max - min) + min);
+  };
 
-  if (str.length === 0) return hash.toString();
+  const color = stc(str);
 
-  for (i = 0, len = str.length; i < len; i++) {
-    chr = str.charCodeAt(i);
-    // eslint-disable-next-line no-bitwise
-    hash = ((hash << 5) - hash) + chr;
-    // eslint-disable-next-line no-bitwise
-    hash |= 0; // Convert to 32bit integer
+  var r = parseInt(color.substring(1, 3), 16);
+  var g = parseInt(color.substring(3, 5), 16);
+  var b = parseInt(color.substring(5, 7), 16);
+
+  if (r < 100) {
+    r = scaleValue(r, 100, 255);
+  }
+  if (g < 100) {
+    g = scaleValue(g, 100, 255);
+  }
+  if (b < 100) {
+    b = scaleValue(b, 100, 255);
   }
 
-  return hash.toString();
-}
+  const newColor = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+
+  return newColor; // Change the threshold value as per your preference
+};
