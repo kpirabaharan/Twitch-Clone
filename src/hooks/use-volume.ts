8a0b00876b77b194341f80e0547ada8ceddc0719
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-01-28 22:05:16 -0500

    M to Mute
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/hooks/use-volume.ts
+++ src/hooks/use-volume.ts
@@ -1,7 +1,11 @@
 import { RefObject, useState } from 'react';
+import { useEventListener } from 'usehooks-ts';
+
+import { useTyping } from '@/store/use-typing';
 
 export const useVolume = (ref: RefObject<HTMLVideoElement>) => {
   const [volume, setVolume] = useState(100);
+  const { isTyping } = useTyping();
 
   const onVolumeChange = (volume: number) => {
     ref.current!.muted = volume === 0;
@@ -14,6 +18,13 @@ export const useVolume = (ref: RefObject<HTMLVideoElement>) => {
     ref.current!.volume = volume === 0 ? 1 : 0;
     setVolume(volume === 0 ? 100 : 0);
   };
+
+  // Toggle mute with m
+  useEventListener('keydown', (event: KeyboardEvent) => {
+    if (!isTyping && event.key === 'm') {
+      toggleMute();
+    }
+  });
 
   return { volume, onVolumeChange, toggleMute };
 };
